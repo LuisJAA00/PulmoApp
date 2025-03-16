9bdfd6db3a5c46f4292a3e1ad4165a53040d40eb
Luis <luis.alvarezaa@udlap.mx>
2025-03-16 07:07:29 -0600

    nueva anim

--- components/VideoScreen.tsx
+++ components/VideoScreen.tsx
@@ -1,25 +1,80 @@
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef, useState } from "react";
+import { View, StyleSheet, Button, TouchableOpacity } from "react-native";
 import { Video, ResizeMode } from "expo-av";
+import { Text } from "react-native";
 
 const VideoScreen = () => {
+  const videoRef = useRef(null); // Referencia para el video
+  const [isPlaying, setIsPlaying] = useState(false); // Estado para controlar la reproducción
+
+  const handlePlayPause = async () => {
+    if (videoRef.current) {
+      if (isPlaying) {
+        await videoRef.current.setPositionAsync(0);
+        await videoRef.current.pauseAsync();
+      } else {
+        await videoRef.current.playAsync();
+      }
+      setIsPlaying(!isPlaying);
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Video
-        source={require("@/assets/videos/Resp_ver2.mp4")}
+        ref={videoRef} // Asignamos la referencia
+        source={require("@/assets/videos/Anim3.mp4")}
         style={styles.video}
         resizeMode={ResizeMode.COVER}
-        shouldPlay // Reproduce automáticamente
-        isLooping // Se repite en bucle
-        isMuted={true} // Sin sonido
+        isLooping
+        isMuted
       />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity style={styles.button} onPress={handlePlayPause}>
+          <Text style={styles.buttonText}>
+            {isPlaying ? "Reproducir" : "Reiniciar"}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  container: {
+    backgroundColor: "#FFFFFF",
+    padding: 16, // Reduced padding
+    borderRadius: 12,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+    alignItems: "center",
+    width: "90%", // Smaller width for the container
+    alignSelf: "center", // Center the container horizontally
+  },
   video: { width: "100%", height: 300 },
+  timerContainer: {
+    alignItems: "center",
+    marginBottom: 16, // Reduced margin
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    width: "100%",
+  },
+  button: {
+    backgroundColor: "#0f67fe",
+    paddingHorizontal: 16, // Reduced padding
+    paddingVertical: 8, // Reduced padding
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: "#FFFFFF",
+    fontSize: 16, // Slightly smaller font size
+    fontWeight: "bold",
+  },
 });
 
 export default VideoScreen;
